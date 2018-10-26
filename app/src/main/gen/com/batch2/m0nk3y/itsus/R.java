0ed6957d67a39ad4a9fd08d4779d92222e10acf9
Minhaz Rahman <minhaz.rtk@gmail.com>
2018-10-26 23:42:23 +0600

    sync

---
+++ app/src/main/gen/com/batch2/m0nk3y/itsus/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.batch2.m0nk3y.itsus;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
